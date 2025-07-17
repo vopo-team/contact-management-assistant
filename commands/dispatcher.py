d6bc534cb150f2208c955370a43b6253deb6a67f
Vladyslav <108978401+vladyslav-panchenko280@users.noreply.github.com>
2025-07-17 13:31:51 +0300

    feat: create dispatcher class

---
+++ commands/dispatcher.py
@@ -0,0 +1,42 @@
+from .command import Command
+
+class CommandDispatcher:
+    @staticmethod
+    def dispatch(book, name, args):
+        match name:
+            case Command.HELLO:
+                return ''
+            case Command.EXIT | Command.CLOSE:
+                return ''
+            case Command.ADD:
+                return ''
+            case Command.CHANGE:
+                return ''
+            case Command.PHONE:
+                return ''
+            case Command.ALL:
+                return ''
+            case Command.ADD_BIRTHDAY:
+                return ''
+            case Command.BIRTHDAYS:
+                return ''
+            case Command.SHOW_BIRTHDAY:
+                return ''
+            case Command.DELETE:
+                return ''
+            case Command.ADD_ADDRESS:
+                return ''
+            case Command.SHOW_ADDRESS:
+                return ''
+            case Command.ADD_EMAIL:
+                return ''
+            case Command.SHOW_EMAIL:
+                return ''
+            case Command.ADD_NOTE:
+                return ''
+            case Command.SHOW_NOTE:
+                return ''
+            case Command.HELP:
+                return ''
+            case _:
+                return ''
