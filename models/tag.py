1a9e583c261c6c6452d778482b7ccd49a3d43646
Pavlo Horhulenko <149716835+phorhulenko@users.noreply.github.com>
2025-07-17 12:34:15 +0100

    hotfixes tag class - done

--- models/tag.py
+++ models/tag.py
@@ -3,9 +3,13 @@ from field import Field
 class Tag(Field):
     def __init__(self, value):
         str_value = str(value)
-        if not (1 <= len(str_value) <= 30):
-            raise ValueError("Tag must be between 1 and 30 characters long.")
+        self.__validate(str_value)
         super().__init__(str_value)
+
+    @classmethod
+    def __validate(cls, value: str):
+        if not (1 <= len(value) <= 30):
+            raise ValueError("Tag must be between 1 and 30 characters long.")
 
     def __str__(self):
         return f"[{self.value}]"
