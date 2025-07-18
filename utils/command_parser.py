a2ded96337b45515eaddb0f54042345c29c3cd22
Pavlo Horhulenko <149716835+phorhulenko@users.noreply.github.com>
2025-07-18 09:49:28 +0100

    hotfix command parser

--- utils/command_parser.py
+++ utils/command_parser.py
@@ -1,14 +1,7 @@
 class CommandParser:
-    cmd: str
-    args: list[str]
-
     def __init__(self, user_string: str):
         parts = user_string.strip().split()
-        match parts:
-            case []:
-                self.cmd, self.args = '', []
-            case [cmd, *args]:
-                self.cmd, self.args = cmd, args
+        self.cmd, *self.args = parts or [""]
 
     def unpack(self) -> tuple[str, list[str]]:
         return self.cmd, self.args
