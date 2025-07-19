b9716061195f89801efb52000ce0fd578f08945c
Pavlo Horhulenko <149716835+phorhulenko@users.noreply.github.com>
2025-07-19 12:13:21 +0100

    record - crud email - done

--- models/record.py
+++ models/record.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from .name import Name
+from .email import Email
 
 class Record:
     def __init__(self, name: Name):
@@ -9,3 +11,17 @@ class Record:
         self.address = None
         self.note = None
         self.birthday = None
+
+
+      # === Email ===
+    def get_email(self) -> Optional[Email]:
+        return self._email
+
+    def set_email(self, value: Email) -> str:
+        self._email = value
+        return f"Email changed to: {value}"
+
+    def delete_email(self) -> str:
+        old_value = self._email
+        self._email = None
+        return f"Email '{old_value}' deleted."
